793664f4b95667cca5d322562c7dcd60ac04198e
Ivan <104464214+Nestivv@users.noreply.github.com>
2025-08-21 19:33:48 -0400

    Hw3
    Hw3 have been updated.

--- hw03.py
+++ hw03.py
@@ -33,26 +33,57 @@ def get_numbers_ticket(min, max, quantity):
         print ("Ваші лотерейні числа:", ticket)
         return ticket
    
-get_numbers_ticket(2, 4, 2)
+get_numbers_ticket(1, 20, 5)
 
 # Exercise #3
 
 print('-'*30, 'Exercise #3 :', sep='\n')
 
-phone_number = "    +38(050)123-32-34"
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
 
-def normalize_phone(phone_number):
-    pattern = r'\D'
-    repl = ""
-    modified_number = re.sub(pattern, repl, phone_number)
-    if modified_number.startswith('380'):
-        modified_number = '+' + modified_number
-    else:
-        modified_number = '+380' + modified_number
-    modified_number = re.sub(r"(\d{2})(\d{3})(\d{3})(\d{4})", r"\1 (\2) \3-\4", modified_number)
+def normalize_phone(raw_numbers):
+    modified_numbers = []
+    for number in raw_numbers:
+        pattern = r'\D'
+        repl = ""
+        modified_number = re.sub(pattern, repl, number)
+        if modified_number.startswith('380'):
+            modified_number = '+' + modified_number
+        elif modified_number.startswith('0'):
+            modified_number = '+38' + modified_number
+        else:
+            modified_number = '+380' + modified_number
+        modified_number = re.sub(r"(\d{2})(\d{3})(\d{3})(\d{4})", r"\1 (\2) \3-\4", modified_number)
+        modified_numbers.append(modified_number)
 
-    print (modified_number)
+    return modified_numbers
 
-    return modified_number
+modified_numbers = normalize_phone(raw_numbers)
+print ('Option #1:', modified_numbers)
 
-normalize_phone(phone_number)
+print('-'*30, 'Exercise #3 :', sep='\n')
+
+def normalize_phone_2(raw_numbers):
+    modified_numbers = []
+    for number in raw_numbers:
+        pattern = r'\D'
+        repl = ""
+        modified_number = re.sub(pattern, repl, number)
+        modified_number = '+380' + modified_number[-9:]
+        modified_number = re.sub(r"(\d{2})(\d{3})(\d{3})(\d{4})", r"\1 (\2) \3-\4", modified_number)
+        modified_numbers.append(modified_number)
+
+    return modified_numbers
+
+modified_numbers_2 = normalize_phone_2(raw_numbers)
+print ('Option #2:', modified_numbers_2)
