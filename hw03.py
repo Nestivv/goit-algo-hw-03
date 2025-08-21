d8f06aff3389929fcaeb7977005aec3c81491151
Ivan <104464214+Nestivv@users.noreply.github.com>
2025-08-20 23:00:38 -0400

    home work #3
    exc.: #1 and #2 have been updated.

--- hw03.py
+++ hw03.py
@@ -4,13 +4,14 @@ from datetime import datetime, date
 import random, re
 
 print ('Exercise #1 :')
-def get_days_from_today(date):
+def get_days_from_today(user_date):
+    user_date = datetime.strptime(user_date, '%Y-%m-%d').date()
     today = date.today()
-    qty_days = today - date
+    qty_days = today - user_date
     print (f'User date: {user_date}', f'Today: {today}', f'Qty days between them: {qty_days.days}', sep='\n')
     return qty_days.days
 
-user_date = datetime.strptime('2025-09-14', '%Y-%m-%d').date()
+user_date = '2025-09-14'
 
 get_days_from_today(user_date)
 
@@ -18,7 +19,7 @@ get_days_from_today(user_date)
 
 print('-'*30, 'Exercise #2 :', sep='\n')
 def get_numbers_ticket(min, max, quantity):
-    if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int) and min >= 1 and 1 < max <= 1000 and min < quantity < max:
+    if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int) and min >= 1 and min < max <= 1000 and quantity <= max - min:
          ticket = set()
          while len(ticket) < quantity:
              number = random.randrange(min, max, 1)
@@ -32,7 +33,7 @@ def get_numbers_ticket(min, max, quantity):
         print ("Ваші лотерейні числа:", ticket)
         return ticket
    
-get_numbers_ticket(1, 36, 5)
+get_numbers_ticket(2, 4, 2)
 
 # Exercise #3
 
